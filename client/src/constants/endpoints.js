bdd43393b61d87b5fbe9c21c1a30b8f8562989ff
4iar <4iar@users.noreply.github.com>
2016-08-12 15:17:27 +0100

    Cleanup endpoints file formatting

--- client/src/constants/endpoints.js
+++ client/src/constants/endpoints.js
@@ -1,4 +1,7 @@
 export const API_BASE_URL = "https://pinterest-4iar.herokuapp.com";
+
+// Auth
 export const API_AUTH_CALLBACK = API_BASE_URL + '/callback';
-export const API_POSTS_ENDPOINT = API_BASE_URL + '/api/posts';
 export const API_CURRENT_USER_ENDPOINT = API_BASE_URL + '/api/auth/currentuser';
+
+export const API_POSTS_ENDPOINT = API_BASE_URL + '/api/posts';
